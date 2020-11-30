d46fc10a21aea67fca9a8de62b3b4e9417602eb2
zhoudongliang <zhoudongliang@allinmd.cn>
2020-11-30 14:39:19 +0800

    ？？？

--- src/main/java/com/mukundmadhav/springboot/springboot/App.java
+++ src/main/java/com/mukundmadhav/springboot/springboot/App.java
@@ -1,9 +1,12 @@
 package com.mukundmadhav.springboot.springboot;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.SocketChannel;
 import java.util.HashMap;
@@ -18,18 +21,104 @@ import java.util.Set;
  * @date 2020/10/5
  */
 public class App {
+    /**
+     * 测试主函数
+     */
+    public static void main(String[] args) {
+        /**
+         * 创建一个二叉树
+         *    0
+         *  1   2
+         * 3 4 5 6
+         */
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        // 选两个测试节点用例
+        TreeNode nodeA = root.right.left;
+        TreeNode nodeB = root.right.right;
+
+        // 查找
+        TreeNode closestCommonAncestor = findClosestCommonAncestor(root, nodeA, nodeB);
+
+        // 打印结果
+        System.out.println(null == closestCommonAncestor ? "空" : "公共祖先的值为：" + closestCommonAncestor.val);
+    }
+
+    /**
+     * 查找二叉树里节点A和B最近的公共祖先
+     *
+     * @param root  二叉树
+     * @param nodeA 节点A
+     * @param nodeB 节点B
+     * @return 祖先
+     */
+    public static TreeNode findClosestCommonAncestor(TreeNode root, TreeNode nodeA, TreeNode nodeB) {
+        // 按前序遍历为例
+        // 非法数据判断：如果传入的树为空，则直接返回空
+        if (root == null) {
+            return null;
+        }
+        // 边界数据判断：如果传入的两节点是根，则直接返回根
+        if (root == nodeA || root == nodeB) {
+            return root;
+        }
+        // 左子树查找
+        TreeNode left = findClosestCommonAncestor(root.left, nodeA, nodeB);
+        // 右子树查找
+        TreeNode right = findClosestCommonAncestor(root.right, nodeA, nodeB);
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            // 左右子树都不为空
+            return root;
+        }
+    }
+
+
+    /**
+     * 二叉树定义
+     */
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public void zeroCopy() {
+        try {
+            File f = new File("txt");
+            RandomAccessFile raf = new RandomAccessFile(f, "r");
+            FileChannel fc = raf.getChannel();
+            Socket socket = new Socket("127.0.0.1", 1);
+            fc.transferTo(0, raf.length(), socket.getChannel());
+            fc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Created by chengxiao on 2016/12/17.
      * 堆排序demo
      */
-    public static void main(String[] args) {
-        System.out.println(6-4/2);
+//    public static void main(String[] args) {
+//        System.out.println(6-4/2);
 //        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 //        sort(arr);
 //        System.out.println(Arrays.toString(arr));
-    }
-
+//    }
     public static void sort(int[] arr) {
         //1.构建大顶堆
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
