7c2bd54af444ced7b7b6d7f92c4bfa5f5ab27722
zhoudongliang <zhoudongliang@allinmd.cn>
2021-02-02 23:26:22 +0800

    1

--- src/main/java/com/mukundmadhav/springboot/springboot/JianZhiOffer.java
+++ src/main/java/com/mukundmadhav/springboot/springboot/JianZhiOffer.java
@@ -2,11 +2,12 @@ package com.mukundmadhav.springboot.springboot;
 
 public class JianZhiOffer {
     public static void main(String[] args) {
-        int[] nums={3,1,0,4,2};
+        int[] nums = {3, 1, 0, 4, 2};
         int repeatNumber = findRepeatNumber(nums);
         System.out.println(repeatNumber);
         System.out.println(nums);
     }
+
     public static int findRepeatNumber(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int tmp = 0;
@@ -20,5 +21,19 @@ public class JianZhiOffer {
             }
         }
         return -1;
+    }
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        int i = matrix.length - 1, j = 0;
+        while (i >= 0 && j <= matrix[0].length - 1) {
+            if (matrix[i][j] > target) {
+                i--;
+            } else if (matrix[i][j] < target) {
+                j++;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
