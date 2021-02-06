4dd6810161375fc7af6c3c8d1b6b72fef65c66ef
zhoudongliang <zhoudongliang@allinmd.cn>
2021-02-06 17:15:11 +0800

    1

--- src/main/java/com/mukundmadhav/springboot/springboot/JianZhiO.java
+++ src/main/java/com/mukundmadhav/springboot/springboot/JianZhiO.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class JianZhiO {
-    public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) {
+    public int lengthOfLongestSubstring(String inputString) {
+        if (inputString.length() == 0) {
             return 0;
         }
         Map<Character, Integer> map = new HashMap<>();
         int max = 0;
         int left = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                left = Math.max(left, map.get(s.charAt(i)) + 1);
+        for (int i = 0; i < inputString.length(); i++) {
+            if (map.containsKey(inputString.charAt(i))) {
+                left = Math.max(left, map.get(inputString.charAt(i)) + 1);
             }
-            map.put(s.charAt(i), i);
+            map.put(inputString.charAt(i), i);
             max = Math.max(max, i - left + 1);
         }
         return max;
