49928456f338b4027afff43651e79c69f1e89829
zhoudongliang <zhoudongliang@allinmd.cn>
2021-02-06 16:43:17 +0800

    1

--- src/main/java/com/mukundmadhav/springboot/springboot/JianZhiO.java
+++ src/main/java/com/mukundmadhav/springboot/springboot/JianZhiO.java
@@ -1,6 +1,24 @@
 package com.mukundmadhav.springboot.springboot;
 
-public class JianZhiOffer {
+import java.util.HashMap;
+
+public class JianZhiO {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        int left = 0;
+        for(int i = 0; i < s.length(); i ++){
+            if(map.containsKey(s.charAt(i))){
+                left = Math.max(left,map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-left+1);
+        }
+        return max;
+
+    }
+
     public String replaceSpace(String s) {
         char[] chars = s.toCharArray();
         for (int i = 0; i < chars.length; i++) {
